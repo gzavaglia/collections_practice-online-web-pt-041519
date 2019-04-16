2c1f578020744d083e4a44c159091cde121084ae
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-16 16:17:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,7 +51,7 @@ def sum_array(array)
 end 
 
 def add_s(array)
-  array.each_with_index |element, index|
+  array.each_with_index do |element, index|
     if index != 1
       element << "s"
     end 
